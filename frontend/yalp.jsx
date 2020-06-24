29ca1115be50f08d88e4916d120b41dd20a1aec5
Benjamin Giller <bgiller@berkeley.edu>
2020-06-24 00:11:52 -0700

    added reducers, getting state to function on window

--- frontend/yalp.jsx
+++ frontend/yalp.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {signup, login, logout} from './util/session_api_util'
+import {signup, login, logout} from './util/session_api_util';
+import configureStore from './store/store';
+
+const store = configureStore()
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     window.signup = signup;
     window.login = login;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch
 
 
 
