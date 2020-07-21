79cc19c9ba707465d5b479edac9d19f57d1bbd1d
Benjamin Giller <bgiller@berkeley.edu>
2020-07-21 09:17:32 -0700

    aws

--- frontend/components/businesses/business_index_item.jsx
+++ frontend/components/businesses/business_index_item.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const BusinessIndexItem = (props) => (
     <div className="business-index-item-box">
     <Link to={`/businesses/${props.business.id}` } className="business-index-item-name" >{props.business.name}</Link>
-    <p>{props.business.address}</p>
+    <div className="business-index-item-address">{props.business.address}</div>
     {/* <p>Latitude:{props.business.latitude}</p> */}
     {/* <p>Longitude:{props.business.longitude}</p> */}
     </div>
