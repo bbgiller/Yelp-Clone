5284ce2b6d3e746bfd206f8d931717ae8be547cb
Benjamin Giller <bgiller@berkeley.edu>
2020-07-27 17:31:56 -0700

    photo added to index page

--- app/views/api/businesses/index.json.jbuilder
+++ app/views/api/businesses/index.json.jbuilder
@@ -2,6 +2,7 @@
     # debugger
     json.set! business.id do 
         json.partial! 'business', business: business 
+        json.photoUrls business.photos.map { |file| url_for(file) }
     end
 
 end
--- frontend/components/businesses/business_index_item.jsx
+++ frontend/components/businesses/business_index_item.jsx
@@ -5,6 +5,9 @@ const BusinessIndexItem = (props) => (
     <div className="business-index-item-box">
     <Link to={`/businesses/${props.business.id}` } className="business-index-item-name" >{props.business.name}</Link>
     <div className="business-index-item-address">{props.business.address}</div>
+    {/* <div className="business-index-item-address">{props.business.ph}</div> */}
+
+    <Link to={`/businesses/${props.business.id}` } className="business-index-item-name" ><img src={props.business.photoUrls ? props.business.photoUrls[0] : null}/></Link>
     {/* <p>Latitude:{props.business.latitude}</p> */}
     {/* <p>Longitude:{props.business.longitude}</p> */}
     </div>
